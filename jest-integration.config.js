aea4da872e81d045146ef7515d5c70d8e3f86c9e
tugzera <damascenobdm@gmail.com>
2020-09-24 10:36:37 -0300

    chore: add jest scripts

---
+++ jest-integration.config.js
@@ -0,0 +1,3 @@
+var config = require('./jest.config')
+config.testMatch = ["**/*.test.ts"]
+module.exports = config
---
+++ jest-unit-config.js
@@ -0,0 +1,3 @@
+var config = require('./jest.config')
+config.testMatch = ["**/*.spec.ts"]
+module.exports = config
