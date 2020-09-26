773f87ab3401588103c92d8863046f8979c6261d
tugzera <damascenobdm@gmail.com>
2020-09-26 09:20:22 -0300

    feat: ensure Bcrypt is called with the correct values

---
+++ src/infra/criptography/bcrypt-adapter.ts
@@ -0,0 +1,14 @@
+import bcrypt from 'bcrypt'
+
+export class BcryptAdapter {
+    private readonly salt
+
+    constructor(salt: number) {
+        this.salt = salt
+    }
+
+    async hash(value: string): Promise<string> {
+        const hashed = await bcrypt.hash(value, this.salt)
+        return null
+    }
+}
