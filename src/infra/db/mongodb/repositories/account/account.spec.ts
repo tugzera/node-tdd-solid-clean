00247279692591db5a7572b269578fa6f9841f34
tugzera <damascenobdm@gmail.com>
2020-10-05 12:56:04 -0300

    test: ensure AccountMongoRepository returns an account on success

---
+++ src/infra/db/mongodb/repositories/account/account.spec.ts
@@ -0,0 +1,27 @@
+import { MongoHelper } from '../../helpers/mongo-helper'
+import { AccountMongoRepository } from './account'
+
+describe('Account Mongo Repository', () => {
+    beforeAll(async () => {
+        await MongoHelper.connect()
+    })
+
+    afterAll(async () => {
+        await MongoHelper.disconnect()
+    })
+
+    test('Should return Account on success', async () => {
+        const sut = new AccountMongoRepository()
+        const data = {
+            name: "any_name",
+            email: "any_email",
+            password: "any_password"
+        }
+        const response = await sut.add(data)
+        expect(response).toBeTruthy()
+        expect(response.id).toBeTruthy()
+        expect(response.name).toEqual(data.name)
+        expect(response.email).toEqual(data.email)
+        expect(response.password).toEqual(data.password)
+    });
+});
