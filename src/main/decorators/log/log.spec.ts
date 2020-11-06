4d20530b4970ae68a46596f74288b93cd3b3ecbd
tugzera <damascenobdm@gmail.com>
2020-11-06 17:11:46 -0300

    refactor: move methos to a single makeSut factory

--- src/main/decorators/log/log.spec.ts
+++ src/main/decorators/log/log.spec.ts
@@ -2,29 +2,45 @@ import { LogControllerDecorator } from './log'
 import { Controller } from '../../../presentation/protocols';
 import { HttpRequest, HttpResponse } from '../../../presentation/protocols/http'
 
-const makeControllerStub = (): Controller => {
-    class ControllerStub implements Controller {
-        async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-            const httpResponse: HttpResponse = {
-                statusCode: 200,
-                body: {
 
-                },
-            }
-            return Promise.resolve(httpResponse)
+class ControllerStub implements Controller {
+    async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+        const httpResponse: HttpResponse = {
+            statusCode: 200,
+            body: {
+
+            },
         }
+        return Promise.resolve(httpResponse)
     }
+}
+
+interface SutTypes {
+    sut: LogControllerDecorator,
+    controllerStub: ControllerStub
+}
+
+const makeControllerStub = (): Controller => {
+
     return new ControllerStub()
+}
+
+const makeSut = (): SutTypes => {
+    const controllerStub = makeControllerStub()
+    const sut = new LogControllerDecorator(controllerStub)
+    return {
+        sut,
+        controllerStub
+    }
 }
 
 describe('Log Decorator', () => {
     test('Should call controller with the correct values', async () => {
-        const controllerStub = makeControllerStub()
+        const { sut, controllerStub } = makeSut()
         const handleSpy = jest.spyOn(controllerStub, 'handle')
         const httpRequest: HttpRequest = {
             body: {}
         }
-        const sut = new LogControllerDecorator(controllerStub)
         await sut.handle(httpRequest)
         expect(handleSpy).toBeCalledWith(httpRequest)
     });
