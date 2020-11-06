653ead8db2490e7a80f21d72bc7c9f2ba21c7084
tugzera <damascenobdm@gmail.com>
2020-11-06 17:07:59 -0300

    refactor: move controller stub to a factory method

--- src/main/decorators/log/log.spec.ts
+++ src/main/decorators/log/log.spec.ts
@@ -2,28 +2,27 @@ import { LogControllerDecorator } from './log'
 import { Controller } from '../../../presentation/protocols';
 import { HttpRequest, HttpResponse } from '../../../presentation/protocols/http'
 
+const makeControllerStub = (): Controller => {
+    class ControllerStub implements Controller {
+        async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
+            const httpResponse: HttpResponse = {
+                statusCode: 200,
+                body: {
+
+                },
+            }
+            return Promise.resolve(httpResponse)
+        }
+    }
+    return new ControllerStub()
+}
+
 describe('Log Decorator', () => {
     test('Should call controller with the correct values', async () => {
-        class ControllerStub implements Controller {
-            async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
-                const httpResponse: HttpResponse = {
-                    statusCode: 200,
-                    body: {
-
-                    },
-                }
-                return Promise.resolve(httpResponse)
-            }
-        }
-        const controllerStub = new ControllerStub()
+        const controllerStub = makeControllerStub()
         const handleSpy = jest.spyOn(controllerStub, 'handle')
         const httpRequest: HttpRequest = {
-            body: {
-                name: 'any_name',
-                email: 'any_email@mail.com',
-                password: 'any_password',
-                confirm_password: 'any_password'
-            }
+            body: {}
         }
         const sut = new LogControllerDecorator(controllerStub)
         await sut.handle(httpRequest)
