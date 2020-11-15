1caa117387ef840ba9b1943fe913ec3eebe4ce97
tugzera <damascenobdm@gmail.com>
2020-11-15 17:49:45 -0300

    feat: ensure login controller return error on missing params, invalid email format

---
+++ src/presentation/controllers/login/login.ts
@@ -0,0 +1,32 @@
+import { Controller, HttpRequest, HttpResponse, EmailValidator } from "../../protocols";
+import { badRequest } from "../../helpers";
+import { MissingParamError, InvalidParamError } from "../../errors";
+
+export class LoginController implements Controller {
+    private readonly emailValidator: EmailValidator
+
+    constructor(emailValidator: EmailValidator) {
+        this.emailValidator = emailValidator
+    }
+
+    async handle(HttpRequest: HttpRequest): Promise<HttpResponse> {
+        try {
+            const requiredFields = ['email', 'password']
+            const { body } = HttpRequest
+            const { email, password } = body
+            for (const field of requiredFields) {
+                if (!body[field]) {
+                    return badRequest(new MissingParamError(field))
+                }
+            }
+            const isValidEmailFormat = this.emailValidator.isValid(email)
+            if (!isValidEmailFormat) {
+                return badRequest(new InvalidParamError('email'))
+            }
+
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+}
